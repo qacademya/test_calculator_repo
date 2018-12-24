6d6d36998e3a9788568da7f914c6b0c4b5170126
EdMSL <eduard2011rus@gmail.com>
2018-12-24 13:34:14 +0700

    Finished initial configuring Gulp files, added some Sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
@@ -6,18 +6,48 @@ var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+
 var del = require('del');
+var rename = require('gulp-rename');
+var server = require('browser-sync').create();
+
+gulp.task('html', function () {
+  return gulp.src('./source/*.html')
+		.pipe(gulp.dest('build'))
+});
 
 gulp.task('sass', function () {
 	return gulp.src('source/sass/style.scss')
 		.pipe(plumber())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest('build/css'));
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('build/css'))
+		.pipe(server.stream());
 });
 
 gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'sass'))
+gulp.task('refresh', function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task('server', function () {
+  server.init({
+    server: 'build/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('sass'));
+  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+});
+
+gulp.task('build', gulp.series('clean', 'html', 'sass'));
+
+gulp.task('start', gulp.series('build', 'server'));
