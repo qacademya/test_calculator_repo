edaf4a8dcfbc4fa9c0b5c8eb3b372dbbc59dac04
EdMSL <eduard2011rus@gmail.com>
2018-12-25 18:22:23 +0700

    Added editorconfig, edits in style files, start scripting

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,48 @@
 'use strict';
 
 (function () {
-	var calculator = document.querySelector('.calculator');
-	var operationsField = calculator.querySelector('#screen_operation');
-	var lastResultField = calculator.querySelector('#screen_result');
+  var calculator = document.querySelector('.calculator');
+  var buttons = calculator.querySelector('.buttons');
+	var calculationField = calculator.querySelector('#screen_calculation');
+	var numberField = calculator.querySelector('#screen_number');
+  var lastResultField = calculator.querySelector('#screen_result');
+  var mathOperations = ['segmentation', 'multiplication', 'summation', 'subtraction'];
+  var specialOperations = ['clear', 'delete', 'float', 'result'];
+  var isNewOperation;
+  var btnValue;
 
-	function changeOperationsFieldValue() {
-		operationsField.value = 1;
-	}
-	
-	function onDigitOnePress(evt) {
-		evt.preventDefault();
-    window.keyEvents.isDigitOneEvent(evt, changeOperationsFieldValue);
-	}
-	
-	document.addEventListener('keydown', onDigitOnePress);
+  function operationReset() {
+    numberField.value = 0;
+    calculationField.value ='';
+    lastResultField.value ='';
+    isNewOperation = true;
+  }
+
+  function clearDisplayValue(input) {
+    input.value = '';
+  }
+
+	function changeNumberFieldValue(newValue) {
+		numberField.value += newValue;
+  }
+
+  function onButtonClick(evt) {
+    var target = evt.target;
+
+    if (target.tagName !== 'BUTTON') {
+      return;
+    }
+
+    btnValue = target.dataset.type;
+
+    if (isNewOperation) {
+      clearDisplayValue(numberField);
+      isNewOperation = false;
+    }
+
+    changeNumberFieldValue(btnValue);
+  }
+
+  operationReset();
+  buttons.addEventListener('click', onButtonClick);
 })();
